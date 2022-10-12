3dd90bf7a12070134481b569b509c8a4a84a88e3
Robert Dutra <robertdutra10@hotmail.com>
2022-10-12 14:04:13 -0300

    first commit

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,29 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+
+}
